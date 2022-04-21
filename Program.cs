a9c5481a6f1661bd8882452393363cdefe6c26ab
admin <admin@CHETUIWK1144>
2022-04-21 14:52:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace faiz_practice
+{
+    class Program
+    {
+        //polymorphism_methodOverloading
+        public class Animal
+        {
+            public Animal()
+            {
+                Console.WriteLine("Eating...");
+            }
+        }
+        public class basic: Animal
+        {
+            public basic()
+            {
+                Console.WriteLine("Eating  food...");
+            }
+        }
+        public class Dog : Animal
+        {
+            public  Dog()
+            {
+               
+                Console.WriteLine("Eating bread...");
+            }
+        }
+        public class TestOverriding
+        {
+            public static void Main()
+            {
+                Dog d = new Dog();
+                basic obj = new basic();
+             
+            }
+        }
+    }
+}
